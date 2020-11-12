2148f27e5283d6c25076b0068bd1b363a3693c87
hai14894 <hohoanghai14894@gmail.com>
2020-11-13 10:38:41 +1100

    return statement

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace myapp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(cube(2));
+
+        }
+        static void SayHi(string name){
+            Console.WriteLine("hello " + name);
+        }
+
+        static double cube(int num){
+            double result = Math.Pow( num, 3);
+            return result;
         }
     }
 }
